9c6ebf88e0341167abd8bd7df5ee1da7c5463bd7
Rob Clayton <rob1694u@hotmail.com>
2018-03-22 10:34:04 +0000

    install of underscore for filtering hotelname filter test now passing

--- app/middleware/search.js
+++ app/middleware/search.js
@@ -1,21 +1,32 @@
-﻿export function searchHotels(searchParams) {
+﻿import _ from 'underscore';
+
+export function searchHotels(searchParams) {
+
+    const data = [
+        {
+            "Name": "hotelone",
+            "StarRating": 5,
+            "Facilities": ["car park", "pool"]
+        },
+        {
+            "Name": "hoteltwo",
+            "StarRating": 3,
+            "Facilities": ["car park", "gym"]
+        },
+        {
+            "Name": "hotelthree",
+            "StarRating": 3,
+            "Facilities": []
+        }
+    ];
+
+    var results = data;
+
+    if (searchParams.hotelName !== '') {
+        results = _.where(data, { Name: searchParams.hotelName });
+    }
+
     return {
-        searchResults: [
-            {
-                "Name": "hotelone",
-                "StarRating": 5,
-                "Facilities": ["car park", "pool"]
-            },
-            {
-                "Name": "hoteltwo",
-                "StarRating": 3,
-                "Facilities": ["car park", "gym"]
-            },
-            {
-                "Name": "hotelthree",
-                "StarRating": 3,
-                "Facilities": []
-            }
-        ]
+        searchResults: results
     }
 };
