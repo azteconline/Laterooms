999776d1db4a7cb792d5aac80831385deed2eaca
Rob Clayton <rob1694u@hotmail.com>
2018-03-22 09:39:42 +0000

    added h1 tag with wrong title

--- app/components/home.js
+++ app/components/home.js
@@ -5,7 +5,7 @@ export default class Home extends React.Component {
     render() {
         return (
             <div>
-               
+               <h1>Hello</h1>
             </div>
         );
     }
