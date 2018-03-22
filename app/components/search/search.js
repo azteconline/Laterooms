caa16c940a27fafd617f01ac50c63cb5aee6f899
Rob Clayton <rob1694u@hotmail.com>
2018-03-22 11:54:40 +0000

    search component tests added elements to search component for passing tests

--- app/components/search/search.js
+++ app/components/search/search.js
@@ -7,7 +7,28 @@ export default class Search extends React.Component {
     render() {
         return (
             <section>
+                <form>
+                    <label htmlFor="hotelName">Hotel Name</label>
+                    <input id="hotelName" type="text" />
+                    <br />
+                    <label htmlFor="starRating">Star Rating</label>
+                    <select id="starRating">
+                        <option value="">Any</option>
+                        <option value="1">1 star</option>
+                        <option value="2">2 star</option>
+                        <option value="3">3 star</option>
+                        <option value="4">4 star</option>
+                        <option value="5">5 star</option>
+                    </select>
+                    <br />
+                    <label>Facilites</label>
+                    <input type="checkbox" id="carpark" value="car park" /> <label htmlFor="carpark">Car Park</label>
+                    <input type="checkbox" id="pool" value="pool" /> <label htmlFor="pool">Pool</label>
+                    <input type="checkbox" id="gym" value="gym" /> <label htmlFor="gym">Gym</label>
 
+                    <br /><br />
+                    <button>search</button>
+                </form>
             </section>
         );
     }
--- test/search.spec.js
+++ test/search.spec.js
@@ -1,5 +1,6 @@
 ﻿import React from 'react';
 import ReactDOM from 'react-dom';
+import TestUtils from 'react-dom/test-utils';
 import Search from '../app/components/search/search.js';
 import Result from '../app/components/search/result.js';
 
@@ -7,13 +8,51 @@ jest.dontMock('../app/components/search/search.js');
 jest.dontMock('../app/components/search/result.js');
 
 describe('Search Components', () => {
-    it('should render search without crashing', () => {
-        const div = document.createElement('div');
-        ReactDOM.render(<Search />, div);
+
+    describe('Search', () => {
+
+        it('should render search without crashing', () => {
+            const div = document.createElement('div');
+            ReactDOM.render(<Search />, div);
+        })
+
+        it('should contain all required form elements', () => {
+            var search = TestUtils.renderIntoDocument(
+                <Search />
+            );
+
+            var form = TestUtils.findRenderedDOMComponentWithTag(search, 'form');
+
+            var hotelName = TestUtils.scryRenderedDOMComponentsWithTag(search, 'input')[0];
+            expect(hotelName.id).toEqual('hotelName');
+            expect(hotelName.type).toEqual('text');
+
+            var starRating = TestUtils.findRenderedDOMComponentWithTag(search, 'select');
+            expect(starRating.id).toEqual('starRating');
+
+            var carpark = TestUtils.scryRenderedDOMComponentsWithTag(search, 'input')[1];
+            expect(carpark.id).toEqual('carpark');
+            expect(carpark.type).toEqual('checkbox');
+
+            var pool = TestUtils.scryRenderedDOMComponentsWithTag(search, 'input')[2];
+            expect(pool.id).toEqual('pool');
+            expect(pool.type).toEqual('checkbox');
+
+            var gym = TestUtils.scryRenderedDOMComponentsWithTag(search, 'input')[3];
+            expect(gym.id).toEqual('gym');
+            expect(gym.type).toEqual('checkbox');
+
+            var button = TestUtils.findRenderedDOMComponentWithTag(search, 'button');
+            expect(button.textContent).toEqual('search');
+        })
+
     })
 
-    it('should render result without crashing', () => {
-        const div = document.createElement('div');
-        ReactDOM.render(<Result />, div);
+    describe('Result', () => {
+
+        it('should render result without crashing', () => {
+            const div = document.createElement('div');
+            ReactDOM.render(<Result />, div);
+        })
     })
 })
