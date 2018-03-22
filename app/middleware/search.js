c26c50e67396b49bf326b640ab6c4225630ed79f
Rob Clayton <rob1694u@hotmail.com>
2018-03-22 10:04:27 +0000

    simple middleware function to get search results with associated test

---
+++ app/middleware/search.js
@@ -0,0 +1,5 @@
+﻿export function searchHotels(searchParams) {
+    return {
+        searchResults : []
+    }
+};
---
+++ test/search.spec.js
@@ -0,0 +1,16 @@
+﻿jest.dontMock('../app/middleware/Search.js');
+import { searchHotels } from '../app/middleware/Search.js';
+
+describe('Search Middleware', () => {
+    it('should return all results when default params supplied', () => {
+        var request = {
+            hotelName: '',
+            starRating: '',
+            facilities: []
+        }
+
+        var response = searchHotels(request);
+
+        expect(response.searchResults.length).toEqual(3);
+    })
+})
